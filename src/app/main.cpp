24135b0f4514e42bdea8556650b8b0184c903ae8
daihuiyou <daihuiyo@163.com>
2026-05-28 11:02:53 +0800

    Prevent multiple instances via QSharedMemory + toast notification

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,7 +1,10 @@
 #include "app/Application.h"
 #include "platform/windows/dpi/HighDpiManager.h"
+#include "presentation/toast/ToastNotifier.h"
 
 #include <QApplication>
+#include <QSharedMemory>
+#include <QTimer>
 
 int main(int argc, char* argv[])
 {
@@ -11,6 +14,14 @@ int main(int argc, char* argv[])
     QCoreApplication::setOrganizationName("SnapPaste");
     QCoreApplication::setApplicationName("SnapPaste");
     QCoreApplication::setApplicationVersion("0.1.0");
+
+    QSharedMemory singleInstanceGuard("SnapPasteSingleInstance");
+    if (!singleInstanceGuard.create(1)) {
+        auto* notifier = new snappaste::ToastNotifier(&qtApplication);
+        notifier->showMessage(QStringLiteral("\u7A0B\u5E8F\u5DF2\u8FD0\u884C"));
+        QTimer::singleShot(2000, &qtApplication, &QApplication::quit);
+        return qtApplication.exec();
+    }
 
     snappaste::Application application(qtApplication);
     return application.run();
