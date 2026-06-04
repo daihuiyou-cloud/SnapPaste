daihuiyou-cloud/SnapPaste

commit a7c20ec8921ca2d5819185d0202e857d64fec83f

fix: remove pixel info overlay (black shadow near cursor)
Completely removes the semi-transparent black info box that follows the mouse in the editor, showing coordinate and pixel color values. The kInfoBg(rgba(0,0,0,180)) overlay was reported as a distracting black shadow.

src/presentation/editor/AnnotationCanvas.cpp
@@ -16,7 +16,6 @@ namespace {
     const QColor kCanvasBg("#1f2329");
     const QColor kAccentColor("#2fbf9f");
     const QColor kWhite("#ffffff");
-    const QColor kInfoBg(0, 0, 0, 180);
 }
 
 AnnotationCanvas::AnnotationCanvas(QWidget* parent)
@@ -117,10 +116,6 @@ void AnnotationCanvas::wireCallbacks()
         contrast_ = toolManager_.contrast();
         rebuildBackingCache();
         update();
-    };
-
-    toolManager_.onMouseInfoChanged = [this](QPointF pos, QColor color) {
-        emit mouseInfoChanged(pos, color);
     };
 
     // Wire EventHandler default callbacks
@@ -498,16 +493,6 @@ int AnnotationCanvas::cornerRadius() const { return toolManager_.cornerRadius();
 void AnnotationCanvas::setCornerRadius(int radius) { toolManager_.setCornerRadius(radius); }
 bool AnnotationCanvas::gridEnabled() const { return toolManager_.gridEnabled(); }
 void AnnotationCanvas::setGridEnabled(bool enabled) { toolManager_.setGridEnabled(enabled); }
-QPointF AnnotationCanvas::mouseImagePos() const { return toolManager_.mouseImagePos(); }
-QColor AnnotationCanvas::mousePixelColor() const { return toolManager_.mousePixelColor(); }
-const QVector<AnnotationTool>& AnnotationCanvas::recentTools() const { return toolManager_.recentTools(); }
-int AnnotationCanvas::undoCount() const { return toolManager_.undoCount(); }
-int AnnotationCanvas::redoCount() const { return toolManager_.redoCount(); }
-void AnnotationCanvas::selectAnnotation(int index) { toolManager_.selectAnnotation(index); }
-void AnnotationCanvas::deleteAnnotation(int index) { toolManager_.deleteAnnotation(index); }
-void AnnotationCanvas::duplicateAnnotation(int index) { toolManager_.duplicateAnnotation(index); }
-void AnnotationCanvas::swapAnnotations(int i, int j) { toolManager_.swapAnnotations(i, j); }
-void AnnotationCanvas::setAnnotationVisible(int index, bool visible) { toolManager_.setAnnotationVisible(index, visible); }
 
 void AnnotationCanvas::updateBrushCursor()
 {
@@ -626,47 +611,6 @@ void AnnotationCanvas::paintEvent(QPaintEvent* event)
         renderer_.drawTextEditCursor(painter, toolManager_.annotations(),
             toolManager_.editingTextIndex(), toolManager_.cursorPos(),
             toolManager_.preeditString(), toolManager_.fontSize(), zf);
-    }
-    // Pixel info overlay near cursor
-    QPointF mouseImgPos = toolManager_.mouseImagePos();
-    QColor mousePxColor = toolManager_.mousePixelColor();
-    if (!image_.isNull() && mousePxColor.isValid()) {
-        auto dpr = image_.devicePixelRatio();
-        QPoint px(static_cast<int>(mouseImgPos.x() * dpr),
-                  static_cast<int>(mouseImgPos.y() * dpr));
-        QRect imgRect(QPoint(0, 0), image_.size());
-        if (imgRect.contains(px)) {
-            double zf = toolManager_.zoomFactor();
-            QString info = tr("(%1, %2) %3")
-                .arg(static_cast<int>(mouseImgPos.x()),
-                     static_cast<int>(mouseImgPos.y()))
-                .arg(mousePxColor.name(QColor::HexRgb).toUpper());
-            static QFont infoFont = []{ QFont f; f.setPixelSize(11); return f; }();
-            painter.setFont(infoFont);
-            auto textRect = painter.fontMetrics().boundingRect(info);
-            int ox = static_cast<int>(mouseImgPos.x() * zf) + 14;
-            int oy = static_cast<int>(mouseImgPos.y() * zf) - textRect.height() - 6;
-            int overlayW = textRect.width() + 8;
-            int overlayH = textRect.height() + 4;
-            if (ox + overlayW > width()) ox = width() - overlayW - 10;
-            if (oy < 2) oy = static_cast<int>(mouseImgPos.y() * zf) + 14;
-            if (oy + overlayH > height()) oy = static_cast<int>(mouseImgPos.y() * zf) - overlayH - 6;
-            QRect bgRect(ox, oy, overlayW, overlayH);
-            painter.setPen(Qt::NoPen);
-            painter.setBrush(kInfoBg);
-            painter.drawRoundedRect(bgRect, 3, 3);
-            painter.setPen(Qt::white);
-            painter.setBrush(Qt::NoBrush);
-            painter.drawText(bgRect, Qt::AlignCenter, info);
-            // Color swatch
-            int swatchSize = 10;
-            bool swatchRight = bgRect.right() + 4 + swatchSize <= width();
-            QRect swatchRect(swatchRight ? bgRect.right() + 4 : bgRect.left() - swatchSize - 4,
-                             bgRect.center().y() - swatchSize / 2, swatchSize, swatchSize);
-            painter.setPen(Qt::NoPen);
-            painter.setBrush(mousePxColor);
-            painter.drawRoundedRect(swatchRect, 2, 2);
-        }
     }
     renderer_.drawDraftSizeLabel(painter, toolManager_.current(),
         toolManager_.draft(), toolManager_.drawing(), toolManager_.zoomFactor());

src/presentation/editor/AnnotationCanvas.h
@@ -109,8 +109,7 @@ public:
     bool textOutlineEnabled() const { return toolManager_.textOutlineEnabled(); }
     bool mosaicBlurred() const { return toolManager_.mosaicBlurred(); }
 
-    QPointF mouseImagePos() const;
-    QColor mousePixelColor() const;
+
 
     const QVector<AnnotationTool>& recentTools() const;
 
@@ -135,7 +134,7 @@ signals:
     void strokeAlphaChanged(int alpha);
     void arrowStyleChanged(int style);
     void cornerRadiusChanged(int radius);
-    void mouseInfoChanged(QPointF pos, QColor color);
+
     void textPropertiesChanged();
     void cropAspectRatioChanged(double ratio);
     void modified();

src/presentation/editor/AnnotationEventHandler.cpp
@@ -381,26 +381,6 @@ void AnnotationEventHandler::mousePressEvent(QMouseEvent* event)
     }
 }
 
-void AnnotationEventHandler::updateMouseInfo(QMouseEvent* event)
-{
-    const auto& img = toolManager_.image();
-    if (!img.isNull()) {
-        QPointF imgPos(toImage(event->pos()));
-        toolManager_.setMouseImagePos(imgPos);
-        auto dpr = img.devicePixelRatio();
-        QPoint px(static_cast<int>(imgPos.x() * dpr),
-                  static_cast<int>(imgPos.y() * dpr));
-        QRect imgRect(QPoint(0, 0), img.size());
-        if (imgRect.contains(px)) {
-            QRgb pixel = reinterpret_cast<const QRgb*>(img.constScanLine(px.y()))[px.x()];
-            QColor pixelColor = QColor::fromRgba(pixel);
-            toolManager_.setMousePixelColor(pixelColor);
-            if (toolManager_.onMouseInfoChanged)
-                toolManager_.onMouseInfoChanged(imgPos, pixelColor);
-        }
-    }
-}
-
 void AnnotationEventHandler::updateMoveCursor(QMouseEvent* event)
 {
     auto tool = toolManager_.currentTool();
@@ -536,7 +516,6 @@ void AnnotationEventHandler::updateDrawingStroke(QMouseEvent* event)
 
 void AnnotationEventHandler::mouseMoveEvent(QMouseEvent* event)
 {
-    updateMouseInfo(event);
     if (toolManager_.panning()) {
         handleMovePan(event);
         event->accept();

src/presentation/editor/AnnotationEventHandler.h
@@ -56,7 +56,6 @@ public:
 
 private:
     QPoint toImage(QPoint widgetPt) const;
-    void updateMouseInfo(QMouseEvent* event);
     void updateMoveCursor(QMouseEvent* event);
 
     void handlePanningPress(QMouseEvent* event);

src/presentation/editor/AnnotationToolManager.h
@@ -32,7 +32,6 @@ public:
     std::function<void(double)> onCropAspectRatioChanged;
     std::function<void()> onImageEdited;
     std::function<void()> onUpdateRequired;
-    std::function<void(QPointF, QColor)> onMouseInfoChanged;
     std::function<void(QCursor)> onSetCursor;
     std::function<void()> onWindowTitleUpdate;
     std::function<void(const QSize&)> onResizeCanvas;
@@ -167,12 +166,6 @@ public:
     QPoint panStart() const { return panStart_; }
     void setPanStart(const QPoint& p) { panStart_ = p; }
 
-    // --- Mouse info ---
-    QPointF mouseImagePos() const { return mouseImagePos_; }
-    void setMouseImagePos(const QPointF& pos) { mouseImagePos_ = pos; }
-    QColor mousePixelColor() const { return mousePixelColor_; }
-    void setMousePixelColor(const QColor& c) { mousePixelColor_ = c; }
-
     // --- Recent tools / colors ---
     const QVector<AnnotationTool>& recentTools() const { return recentTools_; }
     const QVector<QColor>& recentColors() const { return customColors_; }
@@ -283,10 +276,6 @@ private:
     // Pan
     bool panning_ = false;
     QPoint panStart_;
-
-    // Mouse info
-    QPointF mouseImagePos_;
-    QColor mousePixelColor_;
 
     // Recent
     QVector<AnnotationTool> recentTools_;

commit 4cd1b966a9e242f2df529922f61981f16cbd2779

fix: restore accidentally deleted forwarding methods + remove dead recentTools
Commit a7c20ec accidentally removed 8 forwarding methods (undoCount, redoCount, selectAnnotation, deleteAnnotation, duplicateAnnotation, swapAnnotations, setAnnotationVisible, recentTools) along with the intended mouseImagePos/mousePixelColor removal. Restore the 7 needed for linking and remove the unused recentTools declaration.

src/presentation/editor/AnnotationCanvas.cpp
@@ -493,6 +493,13 @@ int AnnotationCanvas::cornerRadius() const { return toolManager_.cornerRadius();
 void AnnotationCanvas::setCornerRadius(int radius) { toolManager_.setCornerRadius(radius); }
 bool AnnotationCanvas::gridEnabled() const { return toolManager_.gridEnabled(); }
 void AnnotationCanvas::setGridEnabled(bool enabled) { toolManager_.setGridEnabled(enabled); }
+int AnnotationCanvas::undoCount() const { return toolManager_.undoCount(); }
+int AnnotationCanvas::redoCount() const { return toolManager_.redoCount(); }
+void AnnotationCanvas::selectAnnotation(int index) { toolManager_.selectAnnotation(index); }
+void AnnotationCanvas::deleteAnnotation(int index) { toolManager_.deleteAnnotation(index); }
+void AnnotationCanvas::duplicateAnnotation(int index) { toolManager_.duplicateAnnotation(index); }
+void AnnotationCanvas::swapAnnotations(int i, int j) { toolManager_.swapAnnotations(i, j); }
+void AnnotationCanvas::setAnnotationVisible(int index, bool visible) { toolManager_.setAnnotationVisible(index, visible); }
 
 void AnnotationCanvas::updateBrushCursor()
 {

src/presentation/editor/AnnotationCanvas.h
@@ -109,10 +109,6 @@ public:
     bool textOutlineEnabled() const { return toolManager_.textOutlineEnabled(); }
     bool mosaicBlurred() const { return toolManager_.mosaicBlurred(); }
 
-
-
-    const QVector<AnnotationTool>& recentTools() const;
-
     int undoCount() const;
     int redoCount() const;
     int annotationCount() const { return toolManager_.annotationCount(); }